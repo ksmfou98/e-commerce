1e593bdc00e5c1fb8c421b1d69ac15e10c3e6567
ksmfou98 <64634992+ksmfou98@users.noreply.github.com>
2021-01-08 20:03:23 +0900

    상품 페이지 main 상단부 완성

--- js/common.js
+++ js/common.js
@@ -12,6 +12,39 @@ window.addEventListener('DOMContentLoaded',function(){
         $("#top_event").css('display', 'none');
     }
 
+    //////////////////////////////////////////////////////////////////
+    //스르륵 없어지는 효과 /////////////////////////////////////////////
+    // $(document).ready(function(){
+    //     var imgs;
+    //     var img_count;
+        
+
+
+    //     imgs = $("#top_event");
+    //     img_count = imgs.children().length;
+
+
+
+        
+
+    //     $('.top_event_close').click(function () {
+    //         next();
+    //         
+    //     });
+
+
+    //     function next(){
+    //             imgs.animate({
+    //                 top:'-=50px'
+    //             });
+                
+
+    //     }
+        
+        
+    // });
+    
+    //////////////////////////////////////////////////////////////////////
 
 
 
