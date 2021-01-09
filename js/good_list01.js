9a8fd23431e9efc38d5b7330f5432ee941ebac1f
ksmfou98 <64634992+ksmfou98@users.noreply.github.com>
2021-01-10 00:30:30 +0900

    적립금 기능 구현,  취소 버튼 클릭시 상품 갯수 초기화 구현

--- js/good_list01.js
+++ js/good_list01.js
@@ -35,17 +35,23 @@ window.addEventListener('DOMContentLoaded',function(){
             $('.bg_loading').css('display','none');
 
             noBody.removeClass('noBody_on');
+            ///// 닫기 눌렀으면 원래값으로 초기화 시켜줘야됨 ////
+            number = 1;
+            $('.inp').val(1); 
+            $(".num").text(Math.floor(number*cost/1000) + ',' + number*cost%1000);
+            $('.emph').text((number*cost)/20 + '원 적립');
+
             
         });
 
-        var number = $('.inp').val();
+        var number = $('.inp').val();   //수량
         var cost = 1300;   // 상품의 가격을 설정해줘야됨.
 
         $('.down').click(function(){
             if(number > 0){
                 number--;
                 $(".inp").val(number);
-                if(number == 0){
+                if(number == 0){               // 0원이면 0출력
                     $(".num").text(0);
                 }
                 else if((number % 10) == 0){
@@ -58,10 +64,14 @@ window.addEventListener('DOMContentLoaded',function(){
                 }
             }
 
-            if(number < 0){
+            if(number < 0){             //0 이하면 false 반환
                 return false;
             }
+
+
+            $('.emph').text((number*cost)/20 + '원 적립');
             
+           
             
             
         });
@@ -84,7 +94,7 @@ window.addEventListener('DOMContentLoaded',function(){
 
 
             
-           
+            $('.emph').text((number*cost)/20 + '원 적립');
             
 
             
