b3607b8bffd6509b6acee621eb80c4b058582448
ksmfou98 <64634992+ksmfou98@users.noreply.github.com>
2021-01-09 22:27:49 +0900

    상품 클릭시 상품 갯수 조절, 가격조절 구현

--- js/good_list01.js
+++ js/good_list01.js
@@ -4,6 +4,108 @@ window.addEventListener('DOMContentLoaded',function(){
 
 
 
+    $(document).ready(function() {
+
+        var noBody = $('.noBody');
+
+
+
+
+
+
+
+
+
+        $('.btn').click(function(){
+
+            $('#cartPut').css('display','block');
+            $('.bg_loading').css('display','block');
+
+
+           
+            noBody.addClass('noBody_on');
+            
+            
+        });
+
+
+
+        $('.txt_type').click(function(){
+            $('#cartPut').css('display','none');
+            $('.bg_loading').css('display','none');
+
+            noBody.removeClass('noBody_on');
+            
+        });
+
+        var number = $('.inp').val();
+        var cost = 1300;   // 상품의 가격을 설정해줘야됨.
+
+        $('.down').click(function(){
+            if(number > 0){
+                number--;
+                $(".inp").val(number);
+                if(number == 0){
+                    $(".num").text(0);
+                }
+                else if((number % 10) == 0){
+                    
+                    $(".num").text(Math.floor(number*cost/1000) + ',' + '000');
+                }
+                else{
+                
+                $(".num").text(Math.floor(number*cost/1000) + ',' + number*cost%1000);
+                }
+            }
+
+            if(number < 0){
+                return false;
+            }
+            
+            
+            
+        });
+
+
+
+        $('.up').click(function(){
+            number++;
+            $(".inp").val(number);
+            if((number % 10) == 0){
+                $(".num").text(Math.floor(number*cost/1000) + ',' + '000');
+            }
+
+            else{
+                $(".num").text(Math.floor(number*cost/1000) + ',' + number*cost%1000);
+                
+
+            }
+
+
+
+            
+           
+            
+
+            
+        });
+
+
+
+        
+
+
+
+
+        
+        
+
+
+
+
+
+
+       
 
 
 
@@ -17,6 +119,19 @@ window.addEventListener('DOMContentLoaded',function(){
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+    });
 
 
 
