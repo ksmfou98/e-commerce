00ce711d1598fca8b325b9a40026d2edc04da604
이도현 <64634992+ksmfou98@users.noreply.github.com>
2021-01-24 00:06:03 +0900

    Add files via upload

--- js/category_moobile.js
+++ js/category_moobile.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-    $('li[type=mobile]').click(function(){             // 위에꺼 줄인거 성공!
+    $('li[type=mobile]').click(function(){             
         var _index =$('li[type=mobile]').index(this);
         index = _index+1;   // _index 값과 nth-child 값이 1 차이가 나서 똑같이 하기 위한 작업
         $('#gnbMenu .list_gnb li:nth-child('+ index +') div').toggle();   // 메뉴가 보였다 안보였다 하는거
