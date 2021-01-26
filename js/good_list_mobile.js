6c8e2c321ae59937f4b2f0016a19bfa6f162fd84
이도현 <64634992+ksmfou98@users.noreply.github.com>
2021-01-26 11:08:40 +0900

    goods_list_mobile update

--- js/good_list_mobile.js
+++ js/good_list_mobile.js
@@ -6,11 +6,11 @@ $(document).ready(function(){
         var cate_index = $('li[name=cate_gory]').index(this);  // 변수 cate_index에다 , 현재 클릭한 name=category의 인덱스를 저장
         
 
-        for(i=0;i<$('.inner_lnb ul li').length;i++){           // li의 갯수만큼 반복해서 on이라는 클래스를 가지고 있는 애를 찾아서 on 삭제
-            if($('.inner_lnb ul li a').hasClass("on") === true){      // 여기서 on은 글자색 보라색으로 해주는 css클래스요소
-                $('.inner_lnb ul li a').removeClass('on');            // 기존에 있던 on은 비활성화 시키고 , 이번에 클릭한 요소에다가 on을 활성화 시키기 위함임
-            }
+        
+        if($('.inner_lnb ul li a').hasClass("on") === true){      // 여기서 on은 글자색 보라색으로 해주는 css클래스요소
+            $('.inner_lnb ul li a').removeClass('on');            // 기존에 있던 on은 비활성화 시키고 , 이번에 클릭한 요소에다가 on을 활성화 시키기 위함임
         }
+        
         $('.inner_lnb ul li:eq(' + cate_index + ') a').addClass('on');  // 위에서 기존에있던 on을 비활성화 시켰으니 , 방금 클릭한 얘 한테 on을 활성화 해줌
 
         var change_bg = $('.inner_lnb ul li:eq(' + cate_index + ') a').innerWidth();  //change_bg라는 변수에다가 방금 클릭한 a태그의 가로길이 값을 저장(막대 바 크기 조절을 위해)
@@ -36,12 +36,25 @@ $(document).ready(function(){
             $('.bg').css('width', change_bg+'px');  // 막대길이는 위에서구한 change_bg만큼 변화
         });
 
-    
 
-        
+    });
+
+    $('.sort_menu .name_select').click(function(){ // name_select 클릭하면 checked 클래스 추가/삭제 
+        $('.sort_menu .user_sort').toggleClass('checked');
+    });
+
+    $('#goodsList .user_sort .list li').click(function(){
+        var _index = $('#goodsList .user_sort .list li').index(this);
+                  
+        if($('#goodsList .user_sort .list li a').hasClass("on") === true){      // 여기서 on은 글자색 보라색으로 해주는 css클래스요소
+            $('#goodsList .user_sort .list li a').removeClass('on');            // 기존에 있던 on은 비활성화 시키고 , 이번에 클릭한 요소에다가 on을 활성화 시키기 위함임
+        }
+       
+        $('#goodsList .user_sort .list li:eq(' + _index + ') a').addClass('on'); 
+
+    });
 
 
-    })
 
    
 
