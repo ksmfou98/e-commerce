451deee6954c404979ce8d40f23d7e32890af031
이도현 <64634992+ksmfou98@users.noreply.github.com>
2021-04-05 14:31:53 +0900

    Header, Login, Join UI 완성

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import Join from "page/Join/Join";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "App.css"
 import Header from "layouts/Header";
+import Login from "page/Login/Login";
+import Main from "page/Main/Main";
 
 function App() {
   return (
     <Router>
       <div id="container">
-        
+        <Header />
 
         <div id="main">
           <div id="content">
-            <div className="page_aticle">
+              <Route exact path="/" component={Main} />
               <Route path="/join" component={Join} />
-            </div>
+              <Route path="/login" component={Login} />
           </div>
         </div>
       </div>
---
+++ src/page/Main/Main.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Main.css"
+
+
+const Main = () => {
+    return(
+        <div className="page-aticle">
+            <div className="main-section1">
+                <div className="main-introduction">
+                    <img src="" alt=""/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Main
